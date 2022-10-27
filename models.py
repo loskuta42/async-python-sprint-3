4b63e954367612c5c2bd23de9ec04f8839b0b1dd
Alexey <loskuta42@yandex.ru>
2022-10-27 20:05:34 +0300

    add models

---
+++ models.py
@@ -0,0 +1,77 @@
+import os
+
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, SmallInteger, Boolean, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql import func
+from sqlalchemy_utils.types.choice import ChoiceType
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'data.sqlite'), echo=True)
+
+Base = declarative_base()
+
+
+class Message(Base):
+    __tablename__ = 'massages'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    text = Column(Text(length=255), nullable=False)
+    pub_date = Column(DateTime(timezone=True), server_default=func.now())
+    author_id = Column(Integer, ForeignKey('users.id'))
+    chat_id = Column(Integer, ForeignKey('chats.id'))
+    comments = relationship('Comment', backref='massage', cascade="all, delete")
+    warnings = Column(SmallInteger, default=0)
+    banned = Column(Boolean)
+    banned_till = Column(DateTime(timezone=True))
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    massage_id = Column(Integer, ForeignKey('massages.id'))
+    author_id = Column(Integer, ForeignKey('users.id'))
+    text = Column(Text(length=255), nullable=False)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __str__(self):
+        return self.text[:15]
+
+
+users_chats = Table(
+    'users_chats',
+    Base.metadata,
+    Column('chat_id', ForeignKey('chats.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True)
+)
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String, unique=True, nullable=False)
+    token = Column(String, unique=True, nullable=False)
+    comments = relationship('Comment', backref='author', lazy='dynamic', cascade="all, delete")
+    messages = relationship('Message', backref='author', lazy='dynamic', cascade="all, delete")
+    chats = relationship('Chat', secondary=users_chats, back_populates='users')
+
+    def __str__(self):
+        return self.user_name
+
+
+class Chat(Base):
+    TYPES = [
+        ('private', 'Private'),
+        ('chanel', 'Chanel'),
+        ('public', 'Public')
+    ]
+    __tablename__ = 'chats'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(ChoiceType(TYPES))
+    name = Column(String, nullable=False)
+    messages = relationship('Message', backref='chat')
+    users = relationship('User', secondary=users_chats, back_populates='chats', lazy='dynamic')
+
+
+Base.metadata.create_all(engine)
--- protocol.py
+++ protocol.py
@@ -2,6 +2,7 @@ import asyncio
 import csv
 import json
 import logging
+import secrets
 import sys
 
 import h11
@@ -9,6 +10,23 @@ import h11
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+ERROR_CODE_TO_MESSAGES = {
+    401: 'Unauthorized. Please name yourself, add "user_name" to request body (not empty)'
+         'and/or Enter your Bearer Token in "Authorization" header. '
+         'If you have not have token yet, get it by POST request to endpoint '
+         '"get_token"',
+    400: 'User with this name is already exists'
+}
+
+MESSAGES_FOR_USER = {
+    'type': {
+        'warning': {
+            'had_token': 'You have got token already.',
+            'no_data': 'no request body'
+        }
+    }
+}
 
 
 class HTTPProtocol(asyncio.Protocol):
@@ -26,7 +44,7 @@ class HTTPProtocol(asyncio.Protocol):
             event = self.connection.next_event()
             try:
                 if isinstance(event, h11.Request):
-                    self.send_response(self.connection, event)
+                    self._request_processing(self.connection, event)
                 elif (
                         isinstance(event, h11.ConnectionClosed)
                         or event is h11.NEED_DATA or event is h11.PAUSED
@@ -35,11 +53,11 @@ class HTTPProtocol(asyncio.Protocol):
             except RuntimeError:
                 logger.exception('exception in data_received')
 
-        if self.connection.our_state is h11.DONE: # TODO возможно понадобиться h11.MUST_CLOSE
+        if self.connection.our_state is h11.DONE:  # TODO возможно понадобиться h11.MUST_CLOSE
             logger.info('Close connection %s', self.transport.get_extra_info('peername'))
             self.transport.close()
 
-    def send_response(self, connection, request_event):
+    def _request_processing(self, connection, request_event):
         if request_event.method not in [b'GET', b'POST']:
             logger.error('runtime_error')
             raise RuntimeError('unsupported method')
@@ -54,32 +72,35 @@ class HTTPProtocol(asyncio.Protocol):
                 logger.exception('exception in sending response')
                 break
             if request_event.method == b'POST':
-                if request_event.target == b'/connect':
-                    data =
-                    self._send_response_for_connect_endpoint()
+                data = json.loads(event.data.decode('utf-8'))
+                user_name = data.get('user_name', None)
+                if not user_name:
+                    self._send_error(401)
+                elif request_event.target == b'/get_token':
+                    self._send_token(user_name)
+                elif request_event.target == b'/connect':
+                    try:
+                        chat_name = data['chat_name']
+                    except KeyError:
+                        chat_name = 'public'
+                    self._send_response_for_connect_endpoint(chat_name)
+                elif request_event.target == b'/send':
+                    chat_name = data.get('chat_name', None)
 
-        # body = b"%s %s" % (event.method.upper(), event.target)
-        # headers = [
-        #     ('content-type', 'application/json'),
-        #     ('content-length', str(len(body))),
-        # ]
-        # response = h11.Response(status_code=200, headers=headers)
-        # self.send(response)
-        # self.send(h11.Data(data=body))
-        # self.send(h11.EndOfMessage())
+                    self._send_response_for_send_endpoint(chat_name)
+
+    def _send_response_with_OK_code(self, body: bytes, headers: list):
+        response = h11.Response(status_code=200, headers=headers)
+        self.send(response)
+        self.send(h11.Data(data=body))
+        self.send(h11.EndOfMessage())
 
     def send_response_for_post(self, data_event, request_event):
         body = data_event.data
         d_body = body.decode('utf-8')
         print(json.loads(d_body))
-        headers = [
-            ('content-type', 'application/json'),
-            ('content-length', str(len(body))),
-        ]
-        response = h11.Response(status_code=200, headers=headers)
-        self.send(response)
-        self.send(h11.Data(data=body))
-        self.send(h11.EndOfMessage())
+        headers = self._get_headers_for_json_body(body)
+        self._send_response_with_OK_code(body, headers)
 
     def send(self, event):
         data = self.connection.send(event)
@@ -94,11 +115,11 @@ class HTTPProtocol(asyncio.Protocol):
             for ind, row in enumerate(reader):
                 if ind == messages_number:
                     break
-                when, who, massage = row
+                when, user_name, massage = row
                 temp_dict['massages'].append(
                     {
                         'datetime': when,
-                        'username': who,
+                        'username': user_name,
                         'massage': massage
                     }
                 )
@@ -108,17 +129,59 @@ class HTTPProtocol(asyncio.Protocol):
         response_body_bytes = response_body_unicode.encode('utf-8')
         return response_body_bytes
 
-    def _send_response_for_connect_endpoint(self):
-        body = self._get_public_massages()
-        headers = [
-            ('content-type', 'application/json'),
-            ('content-length', str(len(body))),
-        ]
-        response = h11.Response(status_code=200, headers=headers)
+    def _send_response_for_connect_endpoint(self, chat_name):
+        if not chat_name:
+            body = self._get_public_massages()
+        else:
+            body = self._get_private_massages(user_name)
+        headers = self._get_headers_for_json_body(body)
+        self._send_response_with_OK_code(body, headers)
+
+    def _send_error(self, error_code):
+        body = self._get_one_key_strbody('error', ERROR_CODE_TO_MESSAGES[error_code])
+        headers = self._get_headers_for_json_body(body)
+        response = h11.Response(status_code=error_code, headers=headers)
         self.send(response)
         self.send(h11.Data(data=body))
         self.send(h11.EndOfMessage())
 
+    def _send_info(self, message):
+        body = self._get_one_key_strbody('info', message)
+        headers = self._get_headers_for_json_body(body)
+        self._send_response_with_OK_code(body, headers)
+
+    def _send_token(self, user_name):
+        users = []  # TODO добавить хранилище с пользователями
+        tokens = []  # TODO поставить в соответствие пользователям токены
+
+        if user_name in users:
+            self._send_info(MESSAGES_FOR_USER['type']['warning']['had_token'])
+        token = secrets.token_hex(16)
+        while token in tokens:
+            token = secrets.token_hex(16)
+        tokens.append(token)
+
+        body = self._get_one_key_strbody('token', token)
+        headers = self._get_headers_for_json_body(body)
+        self._send_response_with_OK_code(body, headers)
+
+    @staticmethod
+    def _get_one_key_strbody(key: str, value: str):
+        return json.dumps(
+            {
+                key: value
+            },
+            sort_keys=True,
+            indent=4,
+            separators=(',', ': ')
+        ).encode('uts-8')
+
+    @staticmethod
+    def _get_headers_for_json_body(body):
+        return [
+            ('content-type', 'application/json'),
+            ('content-length', str(len(body))),
+        ]
 
 async def main(host, port):
     loop = asyncio.get_running_loop()
