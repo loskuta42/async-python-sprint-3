5c9c7eb112857890fd03f6b5e2900d6fa5c893ef
Alexey Lobarev <loskuta42@yandex.ru>
2022-11-01 18:48:50 +0300

    done project

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ import yaml
 
 from protocol import HTTPProtocol
 
-
 with open('logging_config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
@@ -16,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 class Server:
     """Custom http server."""
+
     def __init__(self, host: str = '127.0.0.1', port: int = 8000) -> None:
         """
         :param host: server host.
@@ -28,3 +28,8 @@ class Server:
         loop = asyncio.get_event_loop()
         server = await loop.create_server(HTTPProtocol, self.host, self.port)
         await server.serve_forever()
+
+
+if __name__ == '__main__':
+    server_obj = Server()
+    asyncio.run(server_obj.run())
